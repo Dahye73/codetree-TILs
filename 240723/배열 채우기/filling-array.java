18e4257718499403f6711d72085f2f6bf0849396
Dahye73 <102746730+Dahye73@users.noreply.github.com>
2024-07-23 18:15:24 +0900

    Title: 배열 채우기, Time: 145ms, Memory: 10MB, Status: Passed - CodeTree

--- 240723/배열 채우기/filling-array.java
+++ 240723/배열 채우기/filling-array.java
@@ -4,17 +4,24 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);  
         int[] arr = new int[10];
+        boolean check = false;
         for(int i = 0; i<10; i++){
             int a = sc.nextInt();
             if(a == 0){
                 for(int j = i - 1; j>=0; j--){
                     System.out.print(arr[j] + " ");
                 }
+                check = true;
                 break;
             }
             else{
                 arr[i] = a;
             }
-        }        
+        }       
+        if(!check){
+            for(int j = 9; j>=0; j--){
+                    System.out.print(arr[j] + " ");
+                }
+        }
     }
 }
