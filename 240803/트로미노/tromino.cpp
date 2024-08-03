f69b337bbc65cdbbfdc515e934918dc7790d7ccc
Dahye73 <102746730+Dahye73@users.noreply.github.com>
2024-08-03 18:56:10 +0900

    Title: 트로미노, Time: 11ms, Memory: 0MB, Status: Passed - CodeTree

--- 240803/트로미노/tromino.cpp
+++ 240803/트로미노/tromino.cpp
@@ -24,36 +24,30 @@ int main() {
     for(int i = 1; i<=n;i++){
         for(int j =1; j<=m;j++){
             
-            int sum = arr[i][j];
+   
             //가로줄 탐색
-
-            if(j + 1 <=m && j + 2 <=m){
-                for(int k = 1;k<3;k++){
-                    sum += arr[i][j+k];
-                }
+            if(j + 2 <=m){
+                int sum = arr[i][j] + arr[i][j+1] +arr[i][j+2];
+                max_num = max(sum, max_num);
             }
 
-            max_num = max(sum, max_num);
 
-            sum = arr[i][j];
-            //세로줄 탐색
-
-            if(i + 1 <=n && i + 2 <=n){
-                for(int k = 1; k<3;k++){
-                    sum += arr[i+k][j];
-                }
+            // 세로줄 탐색
+            if (i + 2 <= n) { 
+                int sum = arr[i][j] + arr[i + 1][j] + arr[i + 2][j];
+                max_num = max(sum, max_num);
             }
-            max_num = max(sum, max_num);
         
-            // 나머지 탐색
+            // 4가지의 L자 모양 탐색
             for(int k = 0;k<4;k++){
-                sum = arr[i][j];
+            
+                int sum = arr[i][j];
                 
                 int nx = i + dx[k];
                 int ny = j + dy[k];
 
-                int nx2 = i + dx[k+1];
-                int ny2 = j + dy[k+1];
+                int nx2 = i + dx[(k+1) % 4];
+                int ny2 = j + dy[(k+1) % 4];
 
                 if(1 <= nx && nx <= n && 1 <= ny && ny <= m && 1 <= nx2 && nx2 <= n && 1 <= ny2 && ny2 <= m ){
                     sum = sum + arr[nx][ny] + arr[nx2][ny2];
