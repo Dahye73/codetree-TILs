625cfb733834d420dd6d3fcbeeac7f2dd20a3808
Dahye73 <102746730+Dahye73@users.noreply.github.com>
2024-08-03 21:04:14 +0900

    Title: 금 채굴하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240803/금 채굴하기/gold-mining.cpp
+++ 240803/금 채굴하기/gold-mining.cpp
@@ -7,6 +7,13 @@ int main() {
     cin >> n >> m;
     int arr[22][22]; 
 
+    // 배열 초기화
+    for (int i = 0; i < 22; i++) {
+        for (int j = 0; j < 22; j++) {
+            arr[i][j] = 0;
+        }
+    }
+
     // 입력 받기
     for (int i = 1; i <= n; i++) {
         for (int j = 1; j <= n; j++) {
@@ -21,21 +28,19 @@ int main() {
 
     // 모든 가능한 k 값에 대해 탐색
     for (int k = 1; k <= maxK; k++) {
-
         int cost = k * k + (k - 1) * (k - 1);
-        
+
         for (int x = 1; x <= n; x++) {
             for (int y = 1; y <= n; y++) {
-
                 int count = 0;
 
-                for (int i = -k + 1; i < k; i++) {
-        
+                for (int i = -(k+1) + 1; i < k; i++) {
+
                     int width = k - abs(i);
                     for (int j = -width + 1; j < width; j++) {
                         int nx = x + i;
                         int ny = y + j;
-                        // 범위 검사
+                        
                         if (nx >= 1 && nx <= n && ny >= 1 && ny <= n && arr[nx][ny]) {
                             count++;
                         }
